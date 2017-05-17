0a9b379a1f377652ca4d57fed5319693410cd966
张梓雄 <zhangzixiong@1001hao.com>
2017-05-17 15:59:37 +0800

    no message

--- dev-config/server/devServer.js
+++ dev-config/server/devServer.js
@@ -7,16 +7,18 @@ const config = require("./../webpack.config.js");
 const appsConfig = require("../apps.config.js");
 
 new WebpackDevServer(webpack(config), {
-  //设置 WebpackDevServer 的开发目录，默认为当前项目的根目录
+  // 设置 WebpackDevServer 的开发目录，默认为当前项目的根目录
   contentBase: path.join(__dirname, "../../public"),
   publicPath: `/`,
+  proxy: appsConfig.proxy,
+  // 其他配置项
   compress: true,
   hot: true,
   historyApiFallback: true,
   quiet: true,
   noInfo: true,
   stats: { colors: true },
-  proxy: appsConfig.proxy
+  disableHostCheck: true
 }).listen(appsConfig.devServer.port, "0.0.0.0", function(err, result) {
   if (err) {
     return console.log(err);
