baf779a03cfcfebb53c8a64f689252230737e5c6
wxyyxc1992 <384924552@qq.com>
2017-07-27 23:40:34 +0800

    refactor(react)：重构 PostCSS 插件

--- react/dev-config/tool/postcss.config.js
+++ react/dev-config/tool/postcss.config.js
@@ -1,21 +1,14 @@
-module.exports = {
-  plugins: loader => [
-    require("cssnano")({
-      preset: "default"
-    }),
-    require("autoprefixer")({
-      browsers: [
-        "ie >= 9",
-        "ie_mob >= 10",
-        "ff >= 30",
-        "chrome >= 34",
-        "safari >= 7",
-        "opera >= 23",
-        "ios >= 7",
-        "android >= 4.4",
-        "bb >= 10"
-      ]
-    }),
-    require("postcss-flexibility")
-  ]
-};
+/* eslint-disable no-unused-vars */
+
+module.exports = ctx => ({
+  plugins: {
+    cssnano: {
+      preset: 'default'
+    },
+    // 'postcss-flexbugs-fixes': {},
+    autoprefixer: {
+      browsers: ['> 1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9'],
+      flexbox: 'no-2009'
+    }
+  }
+});
