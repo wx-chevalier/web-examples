c2df05ade8a200c2a08f39984965a17ccfe5511e
wxyyxc1992 <384924552@qq.com>
2018-04-03 17:13:18 +0800

    refactor: 更新文件内容

--- webpack/cwa/install/install.bat
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-npm install --global yarn
-yarn install #安装本地package.json中依赖
-yarn global add flow-bin #全局安装flow类型检查工具
-yarn global add cnpm-check #全局安装npm-check
-yarn global add babel-cli #全局安装Babel CLI命令行工具
-yarn global add weinre #全局安装调试工具
